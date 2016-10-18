772d0ee9639ee202b07261a2f71e688ff1cad953
Reuben Turner <reuben.turner42@gmail.com>
2016-10-18 14:48:51 -0400

    10/18 2:48pm see Slack for changes

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,8 @@ angular.module("AppMod", ["ngRoute"])
 	.controller("AppCtrl", ['$http', '$routeParams', '$location', function($http, $routeParams, $location) {
 		var self = this;
 		self.id = $routeParams.memberId;
+		
+		self.today = new Date();
 	
 		// Get all members
 		$http.get('http://localhost:8080/members')
@@ -58,16 +60,60 @@ angular.module("AppMod", ["ngRoute"])
 							self.projects[count].priority = "Critical";
 							break;
 					}
-				}
+					
+				var startDate = new Date(self.projects[count].start_date);
+				var deadline = new Date(self.projects[count].deadline);
+
+                self.projects[count].project_health = self.calcProjHealth(startDate, deadline, self.projects[count].work_remaining);
+			}
 				
 			},function(err) {
 
 			});
 		
-		self.console = function(project){
-			self.projObj.active = project.active,
-			console.log(self.projObj.active)
+		self.calcProjHealth = function(startDate, deadline, work_remaining){
+			var today = new Date();
+			var dDate1 = new Date();
+			if(startDate <= today){
+				dDate1 = today;
+			}
+			else{
+				dDate1 = startDate;
+			}
+			var dDate2 = deadline;
+			var iWeeks, iDateDiff, iAdjust = 0;
+			if (dDate2 < dDate1) return -1; // error code if dates transposed
+			var iWeekday1 = dDate1.getDay(); // day of week
+			var iWeekday2 = dDate2.getDay();
+			iWeekday1 = (iWeekday1 == 0) ? 7 : iWeekday1; // change Sunday from 0 to 7
+			iWeekday2 = (iWeekday2 == 0) ? 7 : iWeekday2;
+			if ((iWeekday1 > 5) && (iWeekday2 > 5)) iAdjust = 1; // adjustment if both days on weekend
+			iWeekday1 = (iWeekday1 > 5) ? 5 : iWeekday1; // only count weekdays
+			iWeekday2 = (iWeekday2 > 5) ? 5 : iWeekday2;
+
+			// calculate differnece in weeks (1000mS * 60sec * 60min * 24hrs * 7 days = 604800000)
+			iWeeks = Math.floor((dDate2.getTime() - dDate1.getTime()) / 604800000)
+
+			if (iWeekday1 <= iWeekday2) {
+				iDateDiff = (iWeeks * 5) + (iWeekday2 - iWeekday1)
+			} else {
+			   iDateDiff = ((iWeeks + 1) * 5) - (iWeekday1 - iWeekday2)
+			}
+
+			iDateDiff -= iAdjust // take into account both days on weekend
+
+			var hoursRemaining = (iDateDiff + 1) * 8;
+			var projectHealth = hoursRemaining / work_remaining * 100;
+			//console.log("Project Health:", projectHealth);
+			// TERNIARY
+			projectHealth > 100 ? projectHealth = 100 : projectHealth = projectHealth;
+			//self.projects[count].project_health = projectHealth;
+			//console.log(self.projects[count]);
+			projectHealth = Math.round(projectHealth);
+			return projectHealth;
 		}
+		
+		
 
 		// MEMBER OBJECT
 		self.memberObj = {
